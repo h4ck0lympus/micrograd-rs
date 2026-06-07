1a9532994ce1a0d36bc5aeb1e6c147c19ee6099e
h4ck0lympus <vishaljuneja707@gmail.com>
2026-06-06 19:01:24 -0700

    backward function implemented for add, mult, and tanh

--- src/engine/value.rs
+++ src/engine/value.rs
@@ -1,5 +1,7 @@
+use std::cell::RefCell;
 use std::fmt;
 use std::ops::{Add, Div, Mul, Sub};
+use std::rc::Rc;
 
 enum Op {
     Add,
@@ -10,60 +12,152 @@ enum Op {
     Noop,
 }
 
-pub struct Value {
+
+struct Val {
     data: f64,
-    parents: Vec<Value>,
     grad: f64,
-    op: Op,
-    backward: fn(),
+    _parents: Vec<Rc<RefCell<Val>>>,
+    _op: Op,
+    _backward: Rc<dyn Fn()>,
 }
+
+#[derive(Clone)]
+pub struct Value(Rc<RefCell<Val>>);
 
 impl Value {
     pub fn new(data:f64) -> Self {
-        Self { 
-            data,
-            parents: Vec<Value>::new(), 
-            grad: 0.0,  
-            op: Op::Noop, 
-            backward: ||{}
-        }
-        
+        Value(Rc::new(RefCell::new(Val {
+            data: data,
+            grad: 0.0,
+            _parents: Vec::new(),
+            _op: Op::Noop,
+            _backward: Rc::new(|| {})
+        })))
+    }
+
+    pub fn data(&self) -> f64 {
+        self.0.borrow().data
+    }
+
+    pub fn get_grad(&self) -> f64 {
+        self.0.borrow().grad
+    }
+
+    pub fn set_grad(&self, grad: f64) {
+        self.0.borrow_mut().grad = grad;
+    }
+
+    pub fn tanh(&self) -> Self {
+        let out = Value(Rc::new(RefCell::new(Val {
+            data: self.data().tanh(),
+            grad: 0.0,
+            _parents: vec![self.0.clone()],
+            _op: Op::Tanh,
+            _backward: Rc::new(|| {})
+        })));
+
+        let out_node = out.0.clone();
+        let left = self.0.clone();
+
+        out.0.borrow_mut()._backward = Rc::new(move || {
+            let out_data = out_node.borrow().data;
+            let out_grad = out_node.borrow().grad;
+            left.borrow_mut().grad += (1.0 - out_data * out_data) * out_grad;
+        });
+
+        out
+    }
+
+    pub fn backward(&self) {
+        (self.0.borrow()._backward)();
     }
 }
 
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Value(data={0:.4})", self.data)
+        write!(f, "Value(data={0:.4}, grad={1:.4})", self.data(), self.get_grad())
     }
 }
 
 impl Add for Value {
     type Output = Value;
     fn add(self, rhs: Self) -> Self::Output {
-        Value::new(self.data + rhs.data)
+        let data = self.data() + rhs.data();
+        let out = Value(Rc::new(RefCell::new(Val {
+            data,
+            grad: 0.0,
+            _parents: vec![self.0.clone(), rhs.0.clone()],
+            _op: Op::Add,
+            _backward: Rc::new(|| {})
+        })));
+
+        let left = self.0.clone();
+        let right = rhs.0.clone();
+        let out_node = out.0.clone();
+
+        out.0.borrow_mut()._backward = Rc::new(move ||{
+            let out_grad = out_node.borrow().grad;
+            left.borrow_mut().grad += 1.0 * out_grad;
+            right.borrow_mut().grad += 1.0 * out_grad;
+        });
+
+        out
+    }
+}
+
+
+impl Mul for Value {
+    type Output = Value;
+    fn mul(self, rhs: Self) -> Self::Output {
+        let data = self.data() * rhs.data();
+        let out = Value(Rc::new(RefCell::new(Val {
+            data,
+            grad: 0.0,
+            _parents: vec![self.0.clone(), rhs.0.clone()],
+            _op: Op::Mul,
+            _backward: Rc::new(|| {})
+        })));
+
+        let left = self.0.clone();
+        let right = rhs.0.clone();
+        let out_node = out.0.clone();
+
+        out.0.borrow_mut()._backward = Rc::new( move || {
+            let out_grad = out_node.borrow().grad;
+            let right_data = right.borrow().data;
+            let left_data = left.borrow().data;
+            left.borrow_mut().grad +=  out_grad * right_data;
+            right.borrow_mut().grad +=  out_grad * left_data;
+        });
+
+        out
     }
 }
 
 impl Sub for Value {
     type Output = Value;
     fn sub(self, rhs: Self) -> Self::Output {
-        Value::new(self.data - rhs.data)
+        let new_val = self + Value::new(-1.0) * rhs;
+        new_val.0.borrow_mut()._op = Op::Sub;
+        new_val
     }
 }
 
-impl Mul for Value {
-    type Output = Value;
-    fn mul(self, rhs: Self) -> Self::Output {
-        Value::new(self.data * rhs.data)
-    }
-}
 
-impl Div for Value { 
+impl Div for Value {
     type Output = Value;
     fn div(self, rhs: Self) -> Self::Output {
-        if rhs.data == 0.0 {
+        if rhs.data() == 0.0 {
             panic!("Division by 0 not allowed")
         }
-        Value::new(self.data / rhs.data)
+        let data = self.data() / rhs.data();
+        Value(Rc::new(RefCell::new(Val {
+            data,
+            grad: 0.0,
+            _parents: vec![self.0.clone(), rhs.0.clone()],
+            _op: Op::Div,
+            _backward: Rc::new(|| {})
+        })))
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use micrograd_rs::engine::value::{Value};
 
 fn main() {
-    println!("Hello, world!");
-    let a = Value::new(3.0);
-    let b = Value::new(2.0);
-    println!("a = {}", a);
-    println!("b = {}", b);
-    println!("a * b = {}", a * b);
-    // println!("a = {}", a);
+    let a = Value::new(2.0);
+    let b = Value::new(-3.0);
+    let c = Value::new(10.0);
+    let f = Value::new(-2.0);
+    let e = a.clone() * b.clone();
+    let d = e.clone() + c.clone();
+    let l = d.clone() * f.clone();
+    l.set_grad(1.0);
+    l.backward();
+    d.backward();
+    f.backward();
+    c.backward();
+    e.backward();
+    a.backward();
+    b.backward();
+    println!("a = {}, b = {}, c = {}", a, b, c);
+    println!("d = {}, e = {}, f = {}", d, e, f);
+    println!("l = {}", l);
 }
-
